9075723e95f27789d1913743c6231d98f4bc4716
xander johnson <alexx.johnson@gmail.com>
2014-02-28 17:02:48 -0800

    Changed converstion utils and added katakana files

--- rules/reverse_json_rules.py
+++ rules/reverse_json_rules.py
@@ -1,14 +1,27 @@
 """
-reverserules.py - reverse a transliteration scheme
+reverse_json_rules.py - reverse a transliteration scheme
+
+You need to run this file with python taking two arguemnts.
+
+1. the first list of json rules
+2. the second list of json rules you want to create
+
+* Make sure you remember to type the full path to the location where you want
+your out file to end up ! 
+
 """
 import json
 import sys
 
 rules = json.load(open(sys.argv[1]))
+outfile_name = sys.argv[2]
 
 new_rules = []
 
 for before, after in rules:
   new_rules.append([after, before])
 
-print json.dumps(new_rules, indent=2)
+outfile = open(outfile_name, "w")
+outfile.write(json.dumps(new_rules, indent=2))
+outfile.close()
+
