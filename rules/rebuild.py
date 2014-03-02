ec08eefb8112609d05ccd5644a4c9629ec0ad000
Patrick Hall <pat@amundo.com>
2014-03-02 13:31:32 -0800

    Standardize directory structure, move test/ to top

---
+++ rules/rebuild.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+# iterate through subdirectories
+for subdir, dirs, files in os.walk('.'):
+  print subdir
+  #for file in files:
+  #  print subdir+'/'+file
+
+  # iterate through .txt files
+
+    # read file name and extract before and after names
+
+    # check to see if appropriate .json files exist
+
+      # if not, generate them
