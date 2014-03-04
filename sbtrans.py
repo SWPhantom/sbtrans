666243fa11fe7c99181c4de8663e4b6c669e279d
Patrick Hall <pat@amundo.com>
2014-03-04 10:50:34 -0800

    working on argparse

--- sbtrans.py
+++ sbtrans.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2.7
 """
 sbtrans.py - apply a set of transliteration rules to text
 
@@ -32,4 +33,22 @@ def process_file(rules_filename, input_filename, output_filename):
   transliterated_text = transliterate(rules, input_text)
   output_handle.write(transliterated_text.encode('utf-8'))
   output_handle.close()
+
+if __name__ == "__main__":
+  import sys
+  from optparse import OptionParser
+
+  parser = OptionParser(description="sbtrans.py - apply transliteration rules to text")
+
+  parser.add_option("-o", "--output", dest="output_file",
+                    help="write transliterated content to OUTPUT_FILE",   metavar="OUTPUT_FILE")
+  parser.add_option("-i", "--input", dest="input_file",
+                    help="read untransliterated content from INPUT_FILE", metavar="INPUT_FILE")
+
+  (options, args) = parser.parse_args()
+
+  parser.print_help()
+  #print options
+  #print args
+
 
